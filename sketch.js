317c3a9e670753624cb02ffeaed1619e9db97e95
clinton-van-arnam <113942027+vanarnamc@users.noreply.github.com>
2022-12-04 16:44:01 -0500

    NEW

--- sketch.js
+++ sketch.js
@@ -6,10 +6,19 @@ var brightnessValues = []
 
 let ready= false;
 
-
-
-
 var n = 0 // so that the array knows 
+
+//text
+let str = 'MEDINA RIVER PRESS TO BEGIN MEDINA RIVER PRESS TO BEGIN ';
+let rotateSpeed=.006;
+let startAngle =    0;     // angle where text should start
+let distanceAngle = 360;   // how far (in degrees) text will go
+
+let radius;                // set dynamically in setup()
+let font;
+
+let sinSpeed1=.006;
+
 
 function preload() {  
   song = loadSound('audio/MedinaRiver.mp3');
@@ -21,20 +30,27 @@ createCanvas(windowWidth,windowHeight);
 amp = new p5.Amplitude();
 amp.smooth(5);
 black = color(0)
-white = color(255)
+white = color(192)
 colors = [black, white];
 
+//text
+radius = min(width,height) / 3;
+ textSize(radius*.15);
+ textAlign(CENTER, BASELINE);
+ song.setVolume(.6)
 
   noStroke();
 }
 
 function draw() { 
-      translate(width/2,height/2);
+      cursor(HAND);
       if (ready){
-      
+      translate(width/2,height/2);
+
+      cursor(ARROW);
       let vol = amp.getLevel();
-      let volAmp= vol*200;
-      scale(vol*20);
+      let volAmp= vol*600;
+      scale(vol*40);
       // ------ area tiling ------
       // count tiles
       var counter = 0;
@@ -88,7 +104,12 @@ function draw() {
         }
       }  
     }
-
+    else{ //TEXT DISPLAY
+      background(192);
+      translate(width/2,height/2);
+      rotate(radians(millis()*rotateSpeed));
+      textDisplay(1);
+    }
 
 } 
 
@@ -103,12 +124,35 @@ function coinFlip() { //selects color
   var n2= noise(frameCount*20)*100;
 
 	if (flip < 50) {
-		fill(112);
+		fill(192);
 	} else {
 		fill(0);
 	}
 
 
+}
+
+function textDisplay(x){
+  // calculate the angle between each letter
+
+  rotate(radians(frameCount));
+  let angleBetweenLetters = radians(distanceAngle) / str.length;
+
+  scale(sin(frameCount*sinSpeed1)*x);
+  // display the text!
+  push();
+  //translate(width/2, height/2);        // move to circle's center
+  rotate(radians(startAngle));         // rotate to where text starts
+  for (let i=0; i<str.length; i++) {   // go through each letter in the text
+    push();
+    rotate(i * angleBetweenLetters);   // rotate to angle
+    translate(0,-radius);              // and translate to edge of circle
+    fill(0);
+    noStroke();
+    text(str[i], 0,0);                 // draw character at location
+    pop();
+  }
+  pop();
 }
 
 function mousePressed() {
